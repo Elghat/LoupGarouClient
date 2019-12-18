76c485bf7685cb855afc07021b70c35363b4db09
Gregoire <gregoire.calonne@live.be>
2019-12-18 12:01:39 +0100

    Started thread
    Juste créer la fonction que vas gérer le thread

--- LoupGarouClient/LoupGarouClient/Header.h
+++ LoupGarouClient/LoupGarouClient/Header.h
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <WinSock2.h>
+#include <thread>
+
 
 
 #pragma warning (disable:4996)
 #pragma comment(lib,"ws2_32.lib") //Winsock Library
 
 #pragma once
+
