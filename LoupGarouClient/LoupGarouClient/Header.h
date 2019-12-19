4e1a605ed3b677f6c491e51e0e0870e50e164fd1
Gregoire <gregoire.calonne@live.be>
2019-12-19 20:56:47 +0100

    Finished pseudo
    Quand le client se connecte il envoie direct son pseudo au serveur

--- LoupGarouClient/LoupGarouClient/Header.h
+++ LoupGarouClient/LoupGarouClient/Header.h
@@ -10,6 +10,22 @@
 #pragma warning (disable:4996)
 #pragma warning (disable:6031)
 #pragma comment(lib,"ws2_32.lib") //Winsock Library
+SOCKET sock;
 
 void Connection();
 void Deconnection();
+
+
+void chatReceiverThread(SOCKET sock, char* buffer)
+{
+	while (true)
+	{
+		recv(sock, buffer, sizeof(buffer), 0);
+		printf("\n%s", buffer);
+	}
+}
+
+void programeExit(void)
+{
+	closesocket(sock);
+}
--- LoupGarouClient/LoupGarouClient/Source.cpp
+++ LoupGarouClient/LoupGarouClient/Source.cpp
@@ -1,8 +1,6 @@
 #include "Header.h"
 
-void chatReceiverThread(SOCKET sock, char* buffer);
-void programeExit(void);
-SOCKET sock;
+
 int main()
 {
 	char pseudo[255];
@@ -15,35 +13,24 @@ int main()
 	
 	printf("Veuillez entrer votre pseudo pour la partie\n");
 	scanf("%s", &pseudo);
-	sin.sin_addr.s_addr = /*INADDR_LOOPBACK*/ inet_addr("127.0.0.1"); // qui définit l'adresse du serveur. Si vous codez un serveur, vous n'avez pas à définir d'adresse, vous utiliseriez donc : sin.sin_addr.s_addr = htonl(INADDR_ANY);
+	sin.sin_addr.s_addr =  inet_addr("127.0.0.1"); // qui définit l'adresse du serveur. Si vous codez un serveur, vous n'avez pas à définir d'adresse, vous utiliseriez donc : sin.sin_addr.s_addr = htonl(INADDR_ANY);
 	sin.sin_family = AF_INET; //  la "famille" du socket, le type si on veut. Pour l'Internet, les programmeurs utilisent généralement AF_INET. 
 	sin.sin_port = htons(9999); // port sur lequel on écoute
 	sock = socket(AF_INET, SOCK_STREAM, 0);
+	//Dès que le client se connecte la première chôse qu'il fait c'est d'envoyer son pseudo
 	if (connect(sock, (SOCKADDR*)&sin, sizeof(sin)))
 	{
 		send(sock, pseudo, sizeof(pseudo), 0);
 	};
-	std::thread chatreceiver(chatReceiverThread, sock, buffer);//Démarre la fonction découte pour le chat
+	//Démarre la fonction découte pour le chat
+	std::thread chatreceiver(chatReceiverThread, sock, buffer);
+	//Boucle infinie pour envoyer des messages
 	while (1)
 	{
 		scanf("%s", &line);
 		send(sock, line, sizeof(line), 0);
 	}
-	closesocket(sock);
+	//Fonction qui s'appelle a la fermeture du programme pour être sur qu'on se déconnecte
 	atexit(programeExit);
 	return 0;
-}
-
-void chatReceiverThread(SOCKET sock, char* buffer)
-{
-	while (true)
-	{
-		recv(sock, buffer, sizeof(buffer), 0);
-		printf("\n%s", buffer);
-	}
-}
-
-void programeExit(void)
-{
-	closesocket(sock);
 }
